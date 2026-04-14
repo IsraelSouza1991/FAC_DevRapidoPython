81b88e4e754cf68687bf6ce264bdb0f3112803e4
Israel Souza <israel.felipe1991@gmail.com>
2026-04-14 05:01:36 -0300

    14/04/2026 - Criação de interface do usuário

--- codigo_10 interface grafica/primeiroCodigo.py
+++ codigo_10 interface grafica/primeiroCodigo.py
@@ -1,16 +1,25 @@
-import pyautogui
-screenWidth, screenHeight = pyautogui.size()
-currentMouseX, currentMouseY = pyautogui.position()
-pyautogui.moveTo(100, 150)
-pyautogui.click()
-pyautogui.click(100, 200)
-pyautogui.move(0, 10)
-pyautogui.doubleClick()
-pyautogui.moveTo(500, 500, duration=2, tween=pyautogui.easeInOutQuad)
-pyautogui.write('Olá, Mundo!', interval=0.25)
-pyautogui.press('esc')
-pyautogui.keyDown('shift')
-pyautogui.press(['left', 'left', 'left', 'left'])
-pyautogui.keyUp('shift')
-pyautogui.hotkey('ctrl', 'c')
-pyautogui.alert('Esta é a mensagem para Tela.')
+import tkinter as tk
+
+def submit():
+    nome = nome_entry.get()
+    email = email_entry.get()
+
+    print("Nome: ", nome)
+    print("Email: ", email)
+
+root = tk.Tk()
+root.title("Formulário de Inscrição")
+
+frame = tk.Frame(root)
+frame.pack(padx=50, pady=20)
+
+nome_entry = tk.Entry(frame)
+nome_entry.grid(row=0, column=1)
+
+email_entry = tk.Entry(frame)
+email_entry.grid(row=1, column=1)
+
+submit_button = tk.Button(frame, text="Submeter", command=submit)
+submit_button.grid(row=2, columnspan=2, pady=10)
+
+root.mainloop()
